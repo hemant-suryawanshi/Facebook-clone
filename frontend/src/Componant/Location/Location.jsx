1d70c86fe7835d898f672b141480b1073fdab85d
Hemant Suryawanshi <97448671+hemant-suryawanshi@users.noreply.github.com>
2022-07-10 17:37:27 +0530

    map Intreration
    the css and the UI need to be devoloped

--- frontend/src/Componant/Location/Location.jsx
+++ frontend/src/Componant/Location/Location.jsx
@@ -1,23 +1,64 @@
-import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Input,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useState } from "react";
 // import "Location.css";
 import { ImLocation } from "react-icons/im";
 const Location = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [location,setLocation ]=useState("")
+  const [url,setUrl]=useState()
+  const[isLocation,issetLocation]=useState(false)
+
+  const setlocationurl=()=>{
+    issetLocation(true)
+    setUrl( `https://maps.google.com/maps?q=${location}&t=&z=13&ie=UTF8&iwloc=&output=embed`)
+  }
 
   return (
     <>
-      <Button  onClick={onOpen}>
+      <Button onClick={onOpen}>
         {" "}
         <ImLocation />
         Location
       </Button>
 
-      <Drawer onClose={onClose} isOpen={isOpen} size='sm' placement="left">
+      <Drawer onClose={onClose} isOpen={isOpen} size="sm" placement="left">
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
-          <DrawerHeader>Hello World</DrawerHeader>
-          <DrawerBody></DrawerBody>
+          <DrawerHeader>
+            Location
+          </DrawerHeader>
+          <DrawerBody>
+            <div class="mapouter">
+              <div class="gmap_canvas">
+                <iframe
+                  width="400"
+                  height="300"
+                  id="gmap_canvas"
+                  src={isLocation?url:"https://maps.google.com/maps?q=dehli&t=&z=13&ie=UTF8&iwloc=&output=embed"}
+                ></iframe>
+              </div>
+            </div>
+           
+          </DrawerBody>
+          <DrawerFooter>
+          <Box id="searchlocationbox">
+              <Input value={location} onChange={(e)=>setLocation(e.target.value)} id="setLocationinputBox" width="350px"/>
+              <Button onClick={()=>setlocationurl()} id="setlocationButton">Set Location</Button>
+            </Box>
+          </DrawerFooter>
         </DrawerContent>
       </Drawer>
     </>
